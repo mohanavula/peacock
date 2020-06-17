54c2fd4f4fb9f6f4d8cb8b206b49b129061a5533
Mohan Avula <mohanavula@yahoo.com>
2020-06-17 15:58:12 +0530

    user authentication

--- app/User.php
+++ app/User.php
@@ -2,21 +2,31 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+use Illuminate\Auth\Authenticatable as AuthenticatableTrait;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+// use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+
+class User extends Eloquent implements Authenticatable
 {
     use Notifiable;
+    use AuthenticatableTrait;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
+
+    protected $connection = 'mongodb';
+    protected $collection = 'users';
+
+    
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'college_id'
     ];
 
     /**
